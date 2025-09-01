864c22d1cad26b1c981b8c26d8dd419d7cb64b21
Franklyn Vasquez <franklynvasquez24@gmail.com>
2025-08-31 23:53:30 -0400

    Updated example

--- examples/LibraryFunction/LibraryFunction.ino
+++ examples/LibraryFunction/LibraryFunction.ino
@@ -1,15 +1,26 @@
 //Include for the sketch only, not for libraries.
-#include <SketchBinder.h>
+#include <AutoBind.h>
 
 #define BAUDRATE 115200
 
 //My library function
+#if __has_include(<SketchBoundLibrary.h>)
 #include <SketchBoundLibrary.h>
-void millis_pinter() {
+#define MyLibrary_Bindable
+#endif
+
+void millis_printer() {
     Serial.print("Pre loop milliseconds: ");
     Serial.println(millis());
 }
-bool successfullyBounded = addSketchBinding(bind_loop, &invokable_get(millis_pinter));
+
+//If user include SketchBinder
+#ifdef MyLibrary_Bindable
+Function<void> millis_printer_function = Function<void>(millis_printer);
+const bool successfullyBounded = addSketchBinding(bind_loop, &millis_printer_function);
+#else
+const bool successfullyBounded = false;
+#endif
 
 void setup() {
     Serial.begin(BAUDRATE);
